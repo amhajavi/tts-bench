e5146610e6727aa51156cd927eec296016e124f8
amir <amirhossein.hajavi@huawei.com>
2026-05-26 14:17:49 -0400

    chors: setting up the first skeleton of the project

---
+++ tts_bench/metrics/base.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+class BaseMetric:
+    def compute(self, audio:np.ndarray, text: str, reference: np.ndarray = None) -> float:
+        # every metric must have this implemented
+        raise NotImplementedError
+    
