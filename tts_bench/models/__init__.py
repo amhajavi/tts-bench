5b523b629097838b3a223514641b2ea97308ecc0
amir <amhajavi@gmail.com>
2026-06-01 15:40:04 +0000

    feat: adding your-tts model to the tts-bench

--- tts_bench/models/__init__.py
+++ tts_bench/models/__init__.py
@@ -1,5 +1,7 @@
 from tts_bench.models.xtts import XTTSWrapper
+from tts_bench.models.yourtts import YourTTSWrapper
 
 MODELS = {
     "xtts": XTTSWrapper,
+    "your-tts": YourTTSWrapper
 }
---
+++ tts_bench/models/yourtts.py
@@ -0,0 +1,16 @@
+from tts_bench.models.base import BaseTTSModel
+
+from TTS.api import TTS
+
+class YourTTSWrapper(BaseTTSModel):
+    
+    sample_rate = 16000
+    
+    def __init__(self):
+        super(YourTTSWrapper).__init__()
+                
+        self.tts_model = TTS("tts_models/multilingual/multi-dataset/your_tts").to(self.device)
+    
+    def synthesize(self, text, voice_sample=None, language="en", **kwargs):
+        wav = self.tts_model.tts(text=text, speaker_wav=voice_sample, language=language)
+        return wav, self.sample_rate
