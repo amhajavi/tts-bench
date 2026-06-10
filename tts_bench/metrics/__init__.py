d0b9cdd1138e85ab52437395b30ecd5d205877d7
amir <amhajavi@gmail.com>
2026-06-10 15:13:47 +0000

    feat: adding cer to the metric using a unified whisper model

--- tts_bench/metrics/__init__.py
+++ tts_bench/metrics/__init__.py
@@ -1,6 +1,7 @@
 # Register metrics here as they are implemented:
-from tts_bench.metrics.text_based import WER
+from tts_bench.metrics.text_based import WER, CER   
 
 METRICS = {
     "wer": WER,
+    "cer": CER,
 }
--- tts_bench/metrics/text_based.py
+++ tts_bench/metrics/text_based.py
@@ -1,20 +1,36 @@
 import numpy as np
+from tts_bench.metrics.base import BaseMetric
 from faster_whisper import WhisperModel
-from jiwer import wer as compute_wer
+from jiwer import wer as compute_wer, cer as compute_cer
 
-class WER:
+model = WhisperModel('base', device="cuda", compute_type="float16")
+
+class WER(BaseMetric):
     
-    def __init__(self, model_size: str = "base"):
-        self.model = WhisperModel(model_size, device="cuda", compute_type="float16")
+    def __init__(self):
+        pass
     
     def compute(self, audio:np.ndarray, text: str, reference: np.ndarray = None) -> float:
         # every metric must have this implemented
         # Transcribe audio using faster-whisper
         audio = np.reshape(audio, (-1,))
-        segments, _ = self.model.transcribe(audio,  language="en")
+        segments, _ = model.transcribe(audio,  language="en")
         transcription = " ".join([segment.text for segment in segments])
         
         # Compute WER between transcription and reference text
         error_rate = compute_wer(text, transcription)
         return error_rate
 
+class CER(BaseMetric):
+    
+    def __init__(self):
+        pass
+    
+    def compute(self, audio:np.ndarray, text: str, reference: np.ndarray = None) -> float:
+        # Transcribe audio using faster-whisper
+        audio = np.reshape(audio, (-1,))
+        segments, _ = model.transcribe(audio,  language="en")
+        transcription = " ".join([segment.text for segment in segments])    
+        # Compute CER between transcription and reference text
+        error_rate = compute_cer(text, transcription)
+        return error_rate
