06bd523727da71fa745d05f9307f0a9d60a1e97f
amir <amhajavi@gmail.com>
2026-06-26 15:07:18 +0000

    feat: implementation of dnsmos metric class

--- tts_bench/metrics/__init__.py
+++ tts_bench/metrics/__init__.py
@@ -1,9 +1,11 @@
 # Register metrics here as they are implemented:
 from tts_bench.metrics.text_based import WER, CER   
 from tts_bench.metrics.utmosv2 import UTMOSV2
+from tts_bench.metrics.dnsmos import DNSMOS
 
 METRICS = {
     "wer": WER,
     "cer": CER,
     "utmosv2": UTMOSV2,
+    "dnsmos": DNSMOS,
 }
